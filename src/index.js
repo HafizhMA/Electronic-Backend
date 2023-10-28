9210c7734e8000bb4321658a751f039513c2507c
HafizhMA <muhammadazhr29@gmail.com>
2023-10-28 19:18:56 +0700

    test respon

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 
+app.get("/api", (req, res) => {
+    res.send("hallo gais");
+});
+
 app.listen(PORT,() =>{
     console.log(`express api running on port ${PORT}`);
 });
