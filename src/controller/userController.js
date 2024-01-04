d348e12cce1c80995096e7f621b05b84b2e0ab8c
HafizhMA <muhammadazhr29@gmail.com>
2024-01-04 14:34:29 +0700

    make login request

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -45,3 +45,75 @@ exports.register = async (req,res) => {
         }
     
 }
+
+// login
+exports.login = async(req,res)=>{
+    const {email, password} = req.body;
+
+    if (!email || !password ){
+        return res.status(400).json({error: 'semua harus diisi'});
+    }
+
+    try {
+        const checkUser = await prisma.user.findUnique({
+            where: {email},
+        });
+
+        if(!checkUser){
+            return res.status(400).json({error: 'email salah'})
+        }
+
+        // Pastikan checkUser tidak kosong sebelum mencoba membandingkan password
+        if (checkUser.password) {
+            const matchPassword = await bcrypt.compare(password, checkUser.password)
+            if (matchPassword) {
+                const token = jwt.sign({ userId: checkUser.id }, secret, { expiresIn: '1h' });
+                return res.json({ message: 'Login berhasil', checkUser, token })
+            } else {
+                return res.status(401).json({ error: 'password salah' })
+            }
+        } else {
+            return res.status(400).json({ error: 'password tidak ditemukan atau kosong' })
+        }
+
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({ error: 'Login gagal' })
+    }
+}
+
+
+// Get user by email
+exports.getUserByEmail = async (req, res) => {
+    const { id } = req.params; // Ambil userEmail dari URL
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                id
+            },
+        });
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'User tidak ditemukan',
+                status: false,
+            });
+        }
+
+        return res.json({
+            message: 'User ditemukan',
+            status: true,
+            user,
+        });
+
+    } catch (error) {
+        console.log('Error dalam mendapatkan informasi user:', error);
+        return res.status(500).json({
+            error: 'Internal server error',
+        });
+    }
+};
+
+
+
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const userController = require("../controller/userController");
 
 router.post("/register", userController.register);
+router.get("/getUserByEmail/:id", userController.getUserByEmail);
+router.post("/login", userController.login);
+
 
 module.exports = router;
