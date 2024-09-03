e5a209df5462303825c97ad66337c7663cf641f9
HafizhMA <muhammadazhr29@gmail.com>
2024-09-03 16:30:40 +0700

    create get alamat endpoint

--- src/routes/checkoutRoutes.js
+++ src/routes/checkoutRoutes.js
@@ -3,6 +3,7 @@ const checkoutController = require("../controller/checkoutController");
 const router = express.Router();
 
 router.post('/createAlamat', checkoutController.postAlamat);
+router.get('/getAlamat', checkoutController.getAlamat);
 
 module.exports = router;
 
