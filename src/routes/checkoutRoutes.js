b09c02df268a0523c1ab4bb69d330e114216269b
HafizhMA <muhammadazhr29@gmail.com>
2024-09-10 16:00:46 +0700

    create endpoint to update provinsi, kota, kodepos and alamat

--- src/routes/checkoutRoutes.js
+++ src/routes/checkoutRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 router.post('/createAlamat', checkoutController.postAlamat);
 router.get('/getAlamat', checkoutController.getAlamat);
 router.patch('/choosenAlamat', checkoutController.setAlamat);
+router.put('/updateAlamat', checkoutController.updateAlamat);
 
 module.exports = router;
 
