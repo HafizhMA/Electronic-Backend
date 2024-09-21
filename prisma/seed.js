49b3b6ef8fad500abbbf8e1f33d84dab6ff18bac
HafizhMA <muhammadazhr29@gmail.com>
2024-09-21 16:58:16 +0700

    change berat field to integer, and create endpoint to get ongkir

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,7 +3,7 @@ const prisma = new PrismaClient()
 
 async function main() {
 
-  const userId = "a786f47a-19ff-4079-bff4-6b1bda1a5ad4";
+  const userId = "cb9b60f4-813e-4aab-99de-86d1b85c4c44";
 
   const existingUser = await prisma.user.findUnique({
     where: {
@@ -16,14 +16,14 @@ async function main() {
     await prisma.product.createMany({
       data: [
         {
-          namaBarang: "Camera toko cihuy",
+          namaBarang: "Camera Toko Fiktif",
           deskripsiBarang: "ini adalah Camera",
           img: "https://images.unsplash.com/photo-1502920917128-1aa500764cbd?auto=format&fit=crop&q=80&w=1770&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
           hargaBarang: 300000,
           quantity: 1,
           size: "tes",
           color: "tes",
-          berat: "tes",
+          berat: 600,
           features: "tes",
           capacity: "tes",
           powerConsumption: "tes",
@@ -33,14 +33,14 @@ async function main() {
           userId: userId
         },
         {
-          namaBarang: "Handphone toko cihuy",
+          namaBarang: "Handphone Toko Fiktif",
           deskripsiBarang: "ini adalah Handphone",
           img: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?auto=format&fit=crop&q=80&w=1780&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
           hargaBarang: 500000,
           quantity: 1,
           size: "tes",
           color: "tes",
-          berat: "tes",
+          berat: 200,
           features: "tes",
           capacity: "tes",
           powerConsumption: "tes",
@@ -50,14 +50,14 @@ async function main() {
           userId: userId
         },
         {
-          namaBarang: "Keyboard toko cihuy",
+          namaBarang: "Keyboard Toko Fiktif",
           deskripsiBarang: "ini adalah Keyboard",
           img: "https://images.unsplash.com/photo-1587829741301-dc798b83add3?auto=format&fit=crop&q=80&w=1765&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
           hargaBarang: 100000,
           quantity: 1,
           size: "tes",
           color: "tes",
-          berat: "tes",
+          berat: 500,
           features: "tes",
           capacity: "tes",
           powerConsumption: "tes",
@@ -67,14 +67,14 @@ async function main() {
           userId: userId
         },
         {
-          namaBarang: "Laptop toko cihuy",
+          namaBarang: "Laptop Toko Fiktif",
           deskripsiBarang: "ini adalah Laptop",
           img: "https://images.unsplash.com/photo-1504707748692-419802cf939d?auto=format&fit=crop&q=80&w=2047&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
           hargaBarang: 1500000,
           quantity: 1,
           size: "tes",
           color: "tes",
-          berat: "tes",
+          berat: 3000,
           features: "tes",
           capacity: "tes",
           powerConsumption: "tes",
@@ -84,14 +84,14 @@ async function main() {
           userId: userId
         },
         {
-          namaBarang: "Monitor toko cihuy",
+          namaBarang: "Monitor Toko Fiktif",
           deskripsiBarang: "ini adalah Monitor",
           img: "https://images.unsplash.com/photo-1527443224154-c4a3942d3acf?auto=format&fit=crop&q=80&w=1770&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
           hargaBarang: 700000,
           quantity: 1,
           size: "tes",
           color: "tes",
-          berat: "tes",
+          berat: 5000,
           features: "tes",
           capacity: "tes",
           powerConsumption: "tes",
@@ -101,14 +101,14 @@ async function main() {
           userId: userId
         },
         {
-          namaBarang: "Mouse toko cihuy",
+          namaBarang: "Mouse Toko Fiktif",
           deskripsiBarang: "ini adalah Mouse",
           img: "https://images.unsplash.com/photo-1527864550417-7fd91fc51a46?auto=format&fit=crop&q=80&w=1767&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
           hargaBarang: 80000,
           quantity: 1,
           size: "tes",
           color: "tes",
-          berat: "tes",
+          berat: 150,
           features: "tes",
           capacity: "tes",
           powerConsumption: "tes",
