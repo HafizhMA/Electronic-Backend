7a68fca654bfffaa713004cbf10c9b474798eace
HafizhMA <muhammadazhr29@gmail.com>
2024-11-04 20:15:54 +0700

    create new migration for user image profile

---
+++ prisma/migrations/20241104131126_default_profile_image_user/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "foto_profile" TEXT NOT NULL DEFAULT 'https://img.freepik.com/premium-vector/question-mark-person-head-icon-as-unknown-secret-anonym-user-profile-doubt-secret-brain-mind_101884-2200.jpg?semt=ais_hybrid';
